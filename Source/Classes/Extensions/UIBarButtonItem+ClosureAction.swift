02a2799f0d255fca2ecea27c3d25a2f17c7d422b
Tadeas Kriz <tadeas@brightify.org>
2016-10-06 15:12:31 +0200

    Add convenience initializers for UIBarButtonItem.

---
+++ Source/Classes/Extensions/UIBarButtonItem+ClosureAction.swift
@@ -0,0 +1,44 @@
+//
+//  UIBarButtonItem+ClosureAction.swift
+//  Reactant
+//
+//  Created by Tadeas Kriz on 06/10/2016.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+/// Extension of UIBarButtonItem, that adds option to use closure instead of target and selector
+extension UIBarButtonItem {
+
+    public convenience init(image: UIImage?, style: UIBarButtonItemStyle, action: (() -> Void)? = nil) {
+        self.init(image: image, style: style, target: nil, action: nil)
+
+        register(action: action)
+    }
+
+    public convenience init(image: UIImage?, landscapeImagePhone: UIImage? = nil, style: UIBarButtonItemStyle,
+                            action: (() -> Void)? = nil) {
+        self.init(image: image, landscapeImagePhone: landscapeImagePhone, style: style, target: nil, action: nil)
+
+        register(action: action)
+    }
+
+    public convenience init(title: String?, style: UIBarButtonItemStyle, action: (() -> Void)? = nil) {
+        self.init(title: title, style: style, target: nil, action: nil)
+
+        register(action: action)
+    }
+
+    public convenience init(barButtonSystemItem systemItem: UIBarButtonSystemItem, action: (() -> Void)? = nil) {
+        self.init(barButtonSystemItem: systemItem, target: nil, action: nil)
+
+        register(action: action)
+    }
+
+    private func register(action: (() -> Void)?) {
+        if let action = action {
+            _ = rx.tap.takeUntil(rx.deallocating).subscribe(onNext: action)
+        }
+    }
+}
