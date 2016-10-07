9b2e1798f4d2a49eff0584ac00c71df12e17a14b
Tadeas Kriz <tadeas@brightify.org>
2016-10-07 11:14:23 +0200

    Move methods from Navigator to UIViewController and UINavigationController.

--- Source/Classes/Navigator.swift
+++ Source/Classes/Navigator.swift
@@ -7,6 +7,67 @@
 import UIKit
 import RxSwift
 
+extension UIViewController {
+    @discardableResult
+    public func present<C: UIViewController>(controller: C, animated: Bool = true) -> Observable<C> {
+        let replay = ReplaySubject<Void>.create(bufferSize: 1)
+        present(controller, animated: animated, completion: { replay.onLast() })
+        return replay.rewrite(with: controller)
+    }
+
+    @discardableResult
+    public func dismiss(animated: Bool = true) -> Observable<Void> {
+        let replay = ReplaySubject<Void>.create(bufferSize: 1)
+        dismiss(animated: animated, completion: { replay.onLast() })
+        return replay
+    }
+}
+
+extension UINavigationController {
+    public func push(controller: UIViewController, animated: Bool = true) {
+        pushViewController(controller, animated: animated)
+    }
+
+    @discardableResult
+    public func pop(animated: Bool = true) -> UIViewController? {
+        return popViewController(animated: animated)
+    }
+
+    @discardableResult
+    public func replace(with controller: UIViewController, animated: Bool = true) -> UIViewController? {
+        var controllers = viewControllers
+        let current = controllers.popLast()
+        controllers.append(controller)
+
+        setViewControllers(controllers, animated: animated)
+
+        return current
+    }
+
+    @discardableResult
+    public func popAllAndReplace(with controller: UIViewController) -> [UIViewController] {
+        let transition = CATransition()
+        transition.duration = 0.5
+        transition.type = kCATransitionMoveIn
+        transition.subtype = kCATransitionFromLeft
+        transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        view.layer.add(transition, forKey: nil)
+
+        let replaced = replaceAll(with: controller, animated: false)
+
+        return replaced
+    }
+
+    @discardableResult
+    public func replaceAll(with controller: UIViewController, animated: Bool = true) -> [UIViewController] {
+        let currentControllers = viewControllers
+
+        setViewControllers([controller], animated: animated)
+        
+        return currentControllers
+    }
+}
+
 public struct Navigator {
     private let navigationController: UINavigationController
 
@@ -14,64 +75,47 @@ public struct Navigator {
         self.navigationController = navigationController
     }
 
+    @discardableResult
     public func present<C: UIViewController>(controller: C, animated: Bool = true) -> Observable<C> {
-        let replay = ReplaySubject<Void>.create(bufferSize: 1)
-        navigationController.present(controller, animated: animated, completion: { replay.onLast() })
-        return replay.rewrite(with: controller)
+        return navigationController.present(controller: controller, animated: animated)
     }
 
+    @discardableResult
     public func dismiss(animated: Bool = true) -> Observable<Void> {
-        let replay = ReplaySubject<Void>.create(bufferSize: 1)
-        navigationController.dismiss(animated: animated, completion: { replay.onLast() })
-        return replay
+        return navigationController.dismiss(animated: animated)
     }
 
     public func push(controller: UIViewController, animated: Bool = true) {
-        navigationController.pushViewController(controller, animated: animated)
+        navigationController.push(controller: controller, animated: animated)
     }
 
+    @discardableResult
     public func pop(animated: Bool = true) -> UIViewController? {
-        return navigationController.popViewController(animated: animated)
+        return navigationController.pop(animated: animated)
     }
 
+    @discardableResult
     public func replace(with controller: UIViewController, animated: Bool = true) -> UIViewController? {
-        var controllers = navigationController.viewControllers
-        let current = controllers.popLast()
-        controllers.append(controller)
-
-        navigationController.setViewControllers(controllers, animated: animated)
-
-        return current
+        return navigationController.replace(with: controller, animated: animated)
     }
 
+    @discardableResult
     public func popAllAndReplace(with controller: UIViewController) -> [UIViewController] {
-        let transition = CATransition()
-        transition.duration = 0.5
-        transition.type = kCATransitionMoveIn
-        transition.subtype = kCATransitionFromLeft
-        transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
-        navigationController.view.layer.add(transition, forKey: nil)
-
-        let replaced = replaceAll(with: controller, animated: false)
-
-        return replaced
+        return navigationController.popAllAndReplace(with: controller)
     }
 
+    @discardableResult
     public func replaceAll(with controller: UIViewController, animated: Bool = true) -> [UIViewController] {
-        let currentControllers = navigationController.viewControllers
-
-        navigationController.setViewControllers([controller], animated: animated)
-        
-        return currentControllers
+        return navigationController.replaceAll(with: controller, animated: animated)
     }
 
     public static func branchedNavigation(controller: UIViewController,
                                           closeButtonTitle: String? = "Close") -> UINavigationController {
         let navigationController = UINavigationController(rootViewController: controller)
         if let closeButtonTitle = closeButtonTitle {
-            controller.navigationItem.leftBarButtonItem = UIBarButtonItem(title: closeButtonTitle, style: .done, target: navigationController, action: #selector(UINavigationController.dismiss(animated:completion:))) // { _ in
-//                navigationController.dismissViewControllerAnimated(true, completion: nil)
-//            }
+            controller.navigationItem.leftBarButtonItem = UIBarButtonItem(title: closeButtonTitle, style: .done) { _ in
+                navigationController.dismiss(animated: true, completion: nil)
+            }
         }
         return navigationController
     }
