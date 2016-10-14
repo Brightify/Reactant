41e9c991a33e1c5082becd8bc2f759c4d9d9457c
Tadeas Kriz <tadeas@brightify.org>
2016-10-14 12:58:23 +0200

    Fix compilation problem in UIStackView+Util.swift.

--- Source/Classes/Extensions/UIStackView+Util.swift
+++ Source/Classes/Extensions/UIStackView+Util.swift
@@ -4,6 +4,8 @@
 //  Created by Maros Seleng on 10/05/16.
 //
 
+import UIKit
+
 public extension UIStackView {
     public func arrangedChildren(_ children: UIView...) -> UIStackView {
         children.forEach(addArrangedSubview)
