47071c41826178e5db0a89eaa0c4cf9457289c06
Filip Dolník <filip.dolnik.96@gmail.com>
2017-03-01 20:36:29 +0100

    Add defaultMessage to ActivityIndicator.

--- Source/ActivityIndicator/ActivityIndicator.swift
+++ Source/ActivityIndicator/ActivityIndicator.swift
@@ -16,20 +16,18 @@ public final class ActivityIndicator: ObservableConvertibleType {
     public let disposeBag = DisposeBag()
     
     private let variable: Variable<[(id: UUID, message: String?)]> = Variable([])
-    
     private let driver: Driver<E>
+    fileprivate let defaultMessage: String?
     
-    public init() {
+    public init(defaultMessage: String? = nil) {
+        self.defaultMessage = defaultMessage
+        
         driver = variable.asDriver()
             .map { (loading: !$0.isEmpty, message: $0.flatMap { $0.message }.first) }
             .distinctUntilChanged { lhs, rhs in lhs.loading == rhs.loading && lhs.message == rhs.message }
     }
     
-    public func trackActivity<O: ObservableConvertibleType>(of source: O, message: String? = nil) -> Observable<O.E> {
-        return trackActivity(of: source, initialMessage: message, messageProvider: { _ in message })
-    }
-    
-    public func trackActivity<O: ObservableConvertibleType>(of source: O, initialMessage: String? = nil, messageProvider: @escaping (O.E) -> String?) -> Observable<O.E> {
+    public func trackActivity<O: ObservableConvertibleType>(of source: O, initialMessage: String?, messageProvider: @escaping (O.E) -> String?) -> Observable<O.E> {
         return Observable.create { [variable] subscriber in
             let observable = source.asObservable().shareReplay(1)
             let subscriptionDisposable = observable.subscribe(subscriber)
@@ -67,13 +65,38 @@ public final class ActivityIndicator: ObservableConvertibleType {
     }
 }
 
+extension ActivityIndicator {
+    
+    public func trackActivity<O: ObservableConvertibleType>(of source: O, message: String?) -> Observable<O.E> {
+        return trackActivity(of: source, initialMessage: message, messageProvider: { _ in message })
+    }
+    
+    public func trackActivity<O: ObservableConvertibleType>(of source: O) -> Observable<O.E> {
+        return trackActivity(of: source, message: defaultMessage)
+    }
+    
+    public func trackActivity<O: ObservableConvertibleType>(of source: O, messageProvider: @escaping (O.E) -> String?) -> Observable<O.E> {
+        return trackActivity(of: source, initialMessage: defaultMessage, messageProvider: messageProvider)
+    }
+}
+
 extension ObservableConvertibleType {
     
-    public func trackActivity(in activityIndicator: ActivityIndicator, message: String? = nil) -> Observable<E> {
+    public func trackActivity(in activityIndicator: ActivityIndicator) -> Observable<E> {
+        return activityIndicator.trackActivity(of: self, message: activityIndicator.defaultMessage)
+    }
+    
+    public func trackActivity(in activityIndicator: ActivityIndicator, message: String?) -> Observable<E> {
         return activityIndicator.trackActivity(of: self, message: message)
     }
     
-    public func trackActivity(in activityIndicator: ActivityIndicator, initialMessage: String? = nil, messageProvider: @escaping (E) -> String?) -> Observable<E> {
+    public func trackActivity(in activityIndicator: ActivityIndicator, messageProvider: @escaping (E) -> String?) -> Observable<E> {
+        return activityIndicator.trackActivity(of: self, initialMessage: activityIndicator.defaultMessage, messageProvider: messageProvider)
+    }
+    
+    public func trackActivity(in activityIndicator: ActivityIndicator, initialMessage: String?, messageProvider: @escaping (E) -> String?) -> Observable<E> {
         return activityIndicator.trackActivity(of: self, initialMessage: initialMessage, messageProvider: messageProvider)
     }
 }
+
+
