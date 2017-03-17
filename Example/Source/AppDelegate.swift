af05665d630803dcfb4d11b307ad303365428cf5
Matous Hybl <hyblmatous@gmail.com>
2017-03-17 17:35:26 +0100

    Add description of controller and root view.

--- Example/Source/AppDelegate.swift
+++ Example/Source/AppDelegate.swift
@@ -15,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = .white
+        window?.makeKeyAndVisible()
+        window?.rootViewController = UINavigationController(rootViewController: MainViewController())
         // Override point for customization after application launch.
         return true
     }
