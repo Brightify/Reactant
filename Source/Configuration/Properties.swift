e4167e940d18c3ecd436ad9ee76d0b2aefaf6c21
Filip Dolník <filip.dolnik.96@gmail.com>
2017-05-01 16:27:39 +0200

    Add documentation for Configuration

--- Source/Configuration/Properties.swift
+++ Source/Configuration/Properties.swift
@@ -10,5 +10,11 @@ public struct Properties {
     
     // Has to be declared here because of https://bugs.swift.org/browse/SR-631 . (Move to Properties+Style when resolved.)
     public struct Style {
+        
+        private init() {
+        }
+    }
+    
+    private init() {
     }
 }
