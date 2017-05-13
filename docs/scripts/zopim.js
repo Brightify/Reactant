bcdcb2cd13d35ff32d28e02b5055ef58d8650fdd
Tadeas Kriz <tadeas@brightify.org>
2017-05-13 19:15:56 +0200

    Add zopim to docs.

---
+++ docs/scripts/zopim.js
@@ -0,0 +1,5 @@
+window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+$.src="https://v2.zopim.com/?4o5d8TVw1Gt95sL69RIn0iWT2V4NSeye";z.t=+new Date;$.
+type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
