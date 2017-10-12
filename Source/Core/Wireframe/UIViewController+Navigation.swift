8e97a1bc3d2228b9fcc0a41fe1600bc422b7a4ec
Matyáš Kříž <kriz.matyas8.10@gmail.com>
2017-10-12 19:00:07 +0200

    UIViewController.

--- Source/Core/Wireframe/UIViewController+Navigation.swift
+++ Source/Core/Wireframe/UIViewController+Navigation.swift
@@ -9,14 +9,23 @@
 import RxSwift
 
 extension UIViewController {
-    
+
+    /**
+     * Presents a view controller and returns `Observable` that indicates when the view has been successfully presented.
+     * - parameter controller: generic controller to present
+     * - parameter animated: determines whether the view controller presentation should be animated, default is `true`
+     */
     @discardableResult
     public func present<C: UIViewController>(controller: C, animated: Bool = true) -> Observable<C> {
         let replay = ReplaySubject<Void>.create(bufferSize: 1)
         present(controller, animated: animated, completion: { replay.onLast() })
         return replay.rewrite(with: controller)
     }
-    
+
+    /**
+     * Dismisses topmost view controller and returns `Observable` that indicates when the view has been dismissed.
+     * - parameter animated: determines whether the view controller dismissal should be animated, default is `true`
+     */
     @discardableResult
     public func dismiss(animated: Bool = true) -> Observable<Void> {
         let replay = ReplaySubject<Void>.create(bufferSize: 1)
