94e59320263f52da4616e9bbbd14cbdc36116042
Matyáš Kříž <kriz.matyas8.10@gmail.com>
2017-10-26 14:02:34 +0200

    Rule docs.

--- Source/Validation/Rule.swift
+++ Source/Validation/Rule.swift
@@ -8,18 +8,54 @@
 
 import Result
 
+/**
+ * Structure used for setting up a validation rule for values of generic type `T`.
+ * ## Example
+ * ```
+ * typealias Human = (name: String, friendly: Bool)
+ *
+ * let people: [Human] = [("Adam", true), ("Eva", false), ("Agnes", true), ("Rob", false)]
+ *
+ * let friendRule = Rule<Human, FriendError>(validate: { human in
+ *   if human.friendly {
+ *     return nil // no error here, we want to be friends
+ *   } else {
+ *     return FriendError.notFriendly
+ *   }
+ * })
+ *
+ * let friends = people.filter { potentialFriend in
+ *   friendRule.test(potentialFriend)
+ * }
+ * ```
+ */
 public struct Rule<T, E: Error> {
-    
+
+    /// Closure used for validating generic value `T`.
     public let validate: (T) -> E?
-    
+
+    /**
+     * Main initializer for `Rule`.
+     * - parameter validate: closure that is later used in `test(_:)` and `run(_:)` to validate generic value `T`
+     */
     public init(validate: @escaping (T) -> E?) {
         self.validate = validate
     }
-    
+
+    /**
+     * Method testing the passed value with the validation closure.
+     * - parameter value: value to be tested
+     * - returns: `Bool`, `true` if validation was successful and `false` otherwise
+     */
     public func test(_ value: T) -> Bool {
         return validate(value) == nil
     }
-    
+
+    /**
+     * Method running the passed value through the validation closure.
+     * - parameter value: value to be tested, must be of generic type `T`
+     * - returns: `Result`, `.success` with the passed value if validation was successful and `.failure` with the error otherwise
+     */
     public func run(_ value: T) -> Result<T, E> {
         if let error = validate(value) {
             return .failure(error)
