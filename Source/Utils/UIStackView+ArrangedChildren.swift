7b24fdfec65fb006c67ecf17e7f7b46ad081bdb6
Matyáš Kříž <kriz.matyas8.10@gmail.com>
2017-10-26 15:05:57 +0200

    Arranged Children docs.

--- Source/Utils/UIStackView+ArrangedChildren.swift
+++ Source/Utils/UIStackView+ArrangedChildren.swift
@@ -10,12 +10,22 @@ import UIKit
 
 @available(iOS 9.0, *)
 extension UIStackView {
-    
+
+    /**
+     * Adds arranged subviews to the stack view that this method is called upon.
+     * Convenience method working the same as `addArrangedSubview(_:)` but letting you pass multiple UIViews at once.
+     * - parameter children: `UIView`s to be added as arranged subviews
+     */
     @discardableResult
     public func arrangedChildren(_ children: UIView...) -> UIStackView {
         return arrangedChildren(children)
     }
-    
+
+    /**
+     * Adds arranged subviews to the stack view that this method is called upon.
+     * Convenience method working the same as `addArrangedSubview(_:)` but letting you pass an array of UIViews.
+     * - parameter children: `UIView`s to be added as arranged subviews
+     */
     @discardableResult
     public func arrangedChildren(_ children: [UIView]) -> UIStackView {
         children.forEach(addArrangedSubview)
