76958c811bee12360dcc40f2f31313b29fb1e3c6
Matyáš Kříž <kriz.matyas8.10@gmail.com>
2017-10-26 13:00:41 +0200

    CollectionViewState docs.

--- Source/CollectionView/CollectionViewState.swift
+++ Source/CollectionView/CollectionViewState.swift
@@ -6,14 +6,44 @@
 //  Copyright © 2016 Brightify. All rights reserved.
 //
 
+/**
+ * Enum suited for usage with table view as a `Component.componentState`.
+ *
+ * `case items([MODEL])` is used for passing the `MODEL` to the table view for setting up the cells
+ *
+ * `case empty(message: String)` is used for showing no cells (or dividers)
+ * and instead putting the `message` value in the center of the screen
+ *
+ * `case loading` shows no cells (or dividers) and instead puts an animated loading indicator to the top of the table view
+ *
+ * ## Example
+ * Example's `componentState` is `[Friend]` and its `RootView` is a table view (`PlainTableView` for example).
+ * ```
+ * override func afterInit() {
+ *   rootView.componentState = .loading
+ * }
+ *
+ * override func update() {
+ *   if componentState.isEmpty {
+ *     rootView.componentState = .empty("You don't have any friends.")
+ *   } else {
+ *     rootView.componentState = .items(componentState)
+ *   }
+ * }
+ * ```
+ * - NOTE: This enum also contains a method for convenient mapping of `.items`' innards to a different type.
+ * This state is `Equatable` as long as `.items`' `MODEL` is `Equatable`.
+ */
 public enum CollectionViewState<MODEL> {
-    
+
     case items([MODEL])
     case empty(message: String)
     case loading
 
     /**
      * Used to transform items from MODEL to generic T of the same enum `CollectionViewState` using provided closure.
+     *
+     * Doesn't affect `case empty(message: String)` or `case loading` in any way.
      */
     public func mapItems<T>(transform: (MODEL) -> T) -> CollectionViewState<T> {
         switch self {
