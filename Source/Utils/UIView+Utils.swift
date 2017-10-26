602cce274f8565c692fde721cbfd37487db9a619
Matyáš Kříž <kriz.matyas8.10@gmail.com>
2017-10-26 13:24:34 +0200

    UIView+Utils docs.

--- Source/Utils/UIView+Utils.swift
+++ Source/Utils/UIView+Utils.swift
@@ -9,18 +9,58 @@
 import UIKit
 
 extension UIView {
-    
+
+    /**
+     * Adds subviews to the view that this method is called upon.
+     * Convenience method working the same as `addSubview(_:)` but letting you pass multiple UIViews at once.
+     * - parameter children: `UIView`s to be added as subviews
+     * ## Example
+     * ```
+     * override func loadView() {
+     *   children(
+     *     titleLabel,
+     *     loginContainer.children(
+     *       loginTextField,
+     *       passwordTextField,
+     *     ),
+     *     passwordRecoveryButton
+     *   )
+     * }
+     * ```
+     */
     @discardableResult
     public func children(_ children: UIView...) -> UIView {
         return self.children(children)
     }
-    
+
+    /**
+     * Adds subviews to the view that this method is called upon.
+     * Convenience method working the same as `addSubview(_:)` but letting you pass multiple UIViews at once.
+     * - parameter children: `UIView`s to be added as subviews
+     * ## Example
+     * ```
+     * override func loadView() {
+     *   children(
+     *     titleLabel,
+     *     loginContainer.children(
+     *       loginTextField,
+     *       passwordTextField,
+     *     ),
+     *     passwordRecoveryButton
+     *   )
+     * }
+     * ```
+     */
     @discardableResult
     public func children(_ children: [UIView]) -> UIView {
         children.forEach(addSubview)
         return self
     }
-    
+
+    /**
+     * Variable pointing to view's superview if there is one or self if there isn't.
+     * Usually used in Controller when passing `Component.componentState` to the corresponding RootView.
+     */
     public var rootView: UIView {
         if let superview = superview {
             return superview.rootView
