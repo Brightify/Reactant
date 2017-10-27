62a5b9824f40745e34b1be16feb71ccb7368fb8a
Matyáš Kříž <kriz.matyas8.10@gmail.com>
2017-10-27 09:49:26 +0200

    Add more tests for Rule validation. Fix previous tests.

--- Tests/Validation/Rule.swift
+++ Tests/Validation/Rule.swift
@@ -16,17 +16,18 @@ class RuleTest: QuickSpec {
         describe("Rule Validation Tests") {
             let permissiveRule = Rule<String?, ValidationError> { _ in nil }
             let restrictiveRule = Rule<String?, ValidationError> { _ in .invalid }
+            let divBy2StringLengthRule = Rule<String, ValidationError> { string in
+                return string.count % 2 == 0 ? nil : .invalid
+            }
             describe("anything should pass permissive rule") {
                 expect(permissiveRule.test("value")).to(beTruthy())
                 expect(permissiveRule.test("")).to(beTruthy())
                 expect(permissiveRule.test(nil)).to(beTruthy())
 
-                if case .success = permissiveRule.run("value") {
-                } else {
+                if case .failure = permissiveRule.run("value") {
                     fail()
                 }
-                if case .success = permissiveRule.run(nil) {
-                } else {
+                if case .failure = permissiveRule.run(nil) {
                     fail()
                 }
             }
@@ -36,11 +37,28 @@ class RuleTest: QuickSpec {
                 expect(restrictiveRule.test(nil)).to(beFalsy())
 
                 if case .success = restrictiveRule.run("value") {
-                } else {
                     fail()
                 }
                 if case .success = restrictiveRule.run(nil) {
-                } else {
+                    fail()
+                }
+            }
+            describe("strings appropriate should pass \"div by 2 string length rule\" and others should not") {
+                expect(divBy2StringLengthRule.test("")).to(beTruthy())
+                expect(divBy2StringLengthRule.test("nilu")).to(beTruthy())
+                expect(divBy2StringLengthRule.test("value")).to(beFalsy())
+                expect(divBy2StringLengthRule.test("valueueueueueueue")).to(beFalsy())
+
+                if case .failure = divBy2StringLengthRule.run("") {
+                    fail()
+                }
+                if case .failure = divBy2StringLengthRule.run("valu") {
+                    fail()
+                }
+                if case .success = divBy2StringLengthRule.run("value") {
+                    fail()
+                }
+                if case .success = divBy2StringLengthRule.run("valueueueueueueue") {
                     fail()
                 }
             }
