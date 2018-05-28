170ef27a3895bc03338e916efc36c387a9d7dd54
Matyáš Kříž <matyas@brightify.org>
2018-05-28 14:25:50 +0200

    Fix copypasta.

--- Source/Core/Styling/Styleable.swift
+++ Source/Core/Styling/Styleable.swift
@@ -80,8 +80,8 @@ extension Styleable {
      *   }
      * }
      *
-     * let emailLabel = UILabel(text: "Email").apply(style: Styles.infoLabel)
-     * let passwordLabel = UILabel(text: "Password").apply(style: Styles.infoLabel)
+     * let emailLabel = UILabel(text: "Email").with(style: Styles.infoLabel)
+     * let passwordLabel = UILabel(text: "Password").with(style: Styles.infoLabel)
      * ```
      */
     public func with(_ style: Style<Self>) -> Self {
